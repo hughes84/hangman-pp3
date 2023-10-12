8090b910291cf2d9cedabf89b4a7ab4f75f4564f
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-10-12 10:40:38 +0100

    Add game details

--- hangman_structure.py
+++ hangman_structure.py
@@ -134,3 +134,29 @@ phases = [
 ]
 
 
+game_details = [
+ # rules
+ 
+"""
+
+
+
+   H A N G M A N H A N G M A N H A N G M A N H A N G M A N H A N G M A N H A H                                                                         
+   A                              "R U L E S"                                A           
+   N                                                                         N
+   G          1.  Hangman is an objective based family game where a          G                                                         
+   M              word is to be guessed using only letters.                  M
+   A          2.  You will have several attempts but can only have 6         A
+   N              incorrect guesses.                                         N
+   H          3.  With each incorrect guess the hangmans body will           H
+   A              populate.                                                  A
+   N          4.  When the hangmans head and full body appear you lose.      N
+   G          5.  For each correct letter the player receives 10pts.         G                             
+   M                                                                         M   
+   A                                                                         A
+   N H A N G M A N H A N G M A N H A N G M A N H A N G M A N H A N G M A N A N    
+
+   
+                                                                                                       
+"""
+]
