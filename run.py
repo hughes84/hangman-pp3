b8f07c83f3287b5f3fdfe855c8dabd2c38d18deb
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-10-12 13:05:50 +0100

    Add relevant comments

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# Import necessary libraries and modules
 from hangman_structure import *
 
 import os
@@ -6,19 +7,22 @@ import random
 from colorama import Fore
 colorama.init(autoreset=True)
 
-# CONSTS
+# Initialize some constants
 player_name = None
-correct_chosen = 10
+correct_chosen = 10 # Score awarded for each correct letter
 gameover_qs = f"""{Fore.BLUE}
 A - TRY AGAIN
 B - FINISH GAME
 """
 
+# Introduction and player name input
 print(f"{Fore.BLUE}T H E\nH A N G M A N")
 print(f"""
 E N T E R   Y O U R   N A M E   B E L O W
                   """)
 
+
+# Function to clear the terminal screen
 def clear_terminal():
     """
     function to clear terminal
@@ -26,9 +30,10 @@ def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+# Main game function
 def game(word_to_guess):
     """
-    Game main function
+    Main game function
     """
     complete_word = "_" * len(word_to_guess)
     chosen = False
@@ -37,12 +42,13 @@ def game(word_to_guess):
     correct_picks = 0
     tries = 7
     score = 0
-    # print(F"{Fore.MAGENTA}\nLET'S PLAY THE HANGMAN GAME!\n")
+    
     print(f"""{Fore.MAGENTA}
     YOUR WORD HAS {len(word_to_guess)} LETTERS""")
-    print(display_gallows(tries))
+    print(display_gallows(tries)) # Display the initial state of the hangman
     word_progress(f"{complete_word}")
     print("\n")
+
     while not chosen and tries > 1:
         print(f"{Fore.RED}\nINCORRECT PICKS:\n{incorrect_picks}\n")
         show_score(score)
@@ -57,19 +63,17 @@ def game(word_to_guess):
         
     
         # Check if the player has already chosen the letter
-        # Or if the letter chosen is not in the word
-        # And if the letter chosen is in the word
         if len(letter_input) == 1 and letter_input.isalpha():
             if letter_input in chosen_letters:
                 print(f"""{Fore.MAGENTA}\n
                 YOU ALREADY ENTERED {letter_input}\n""")
+            # Check if the letter chosen is not in the word
             elif letter_input not in word_to_guess:
                 print(f"{Fore.RED} HARD LUCK {letter_input} IS NOT IN THE WORD")
-                
-                
                 tries -= 1
                 chosen_letters.append(letter_input)
                 incorrect_picks.append(letter_input)
+            # Check if the letter chosen is not in the word
             else:
                 print(f"{Fore.RED} WELL DONE {letter_input} IS IN THE WORD")
                 chosen_letters.append(letter_input)
@@ -92,7 +96,7 @@ def game(word_to_guess):
         
     outcome(chosen, word_to_guess, correct_picks, score)
 
-
+# Function to display the final outcome of the game
 def outcome(chosen, word_to_guess, correct_picks, score):
     """
     Verify if the player completes game
@@ -106,7 +110,7 @@ def outcome(chosen, word_to_guess, correct_picks, score):
 
     show_score(score)
 
-
+# Function to display the current progress in word guessing
 def word_progress(complete_word):
     """
     Prints correct guesses
@@ -114,7 +118,7 @@ def word_progress(complete_word):
     for i in complete_word:
         print(i, end=" ")
 
-
+# Function to display the hangman based on the number of tries left
 def display_gallows(tries):
     """
     Show the Hangman phases at the beginning of the game 
@@ -122,7 +126,7 @@ def display_gallows(tries):
     """
     return phases[tries]
 
-
+# Function to show the player's current score
 def show_score(score):
     """
     Show score throughout the game
@@ -130,7 +134,7 @@ def show_score(score):
     print(f"SCORE: {score}")
 
 
-
+# Main game loop
 def main():
     """
     The game begins with a word that has to be guessed.
@@ -171,6 +175,7 @@ def main():
             That is not a valid option. Please try again.\n""")
             play_game = False
 
+# List of words for the game
 words = [
     "Serendipity",
     "Mellifluous",
@@ -188,7 +193,7 @@ words = [
     "Euphoria"
 ]
 
-
+# Function to get a word to guess from the list of words
 def get_word():
     """
     Get a word to guess from the list of words
@@ -196,6 +201,6 @@ def get_word():
     word_to_guess = random.choice(words)
     return word_to_guess.upper()
 
-
+# Start the game if the script is run as the main program
 if __name__ == '__main__':
     main()
