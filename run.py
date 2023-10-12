2a1008b55475e95ca4c2cf046f9cf3311d98a5ac
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-10-12 10:29:02 +0100

    Add main function with welcome screen and menu functionality

--- run.py
+++ run.py
@@ -2,6 +2,12 @@ import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
 
+player_name = None
+gameover_qs = f"""{Fore.BLUE}
+A - TRY AGAIN
+B - FINISH GAME
+"""
+
 print(f"{Fore.BLUE}T H E\nH A N G M A N")
 print(f"""
 E N T E R   Y O U R   N A M E   B E L O W
@@ -24,3 +30,44 @@ words = [
     "Euphoria"
 ]
 
+def main():
+    """
+    The game begins with a word that has to be guessed.
+    When the game climaxes, the player is given the choice to play again or exit game
+    """
+     # Let player put in own name to play hangman
+    while True:
+        player_name = input(f"\n{Fore.BLUE}NAME:\n>>> ").strip().upper()
+        if len(player_name) < 3:
+            print(f"{Fore.RED}This is not a valid name!")
+            continue
+        else:
+            break
+    print(f"""{Fore.MAGENTA}\n
+    HELLO {player_name}, WELCOME TO THE HANGMAN GAME!\n""")
+    
+    input(f"""\n{Fore.BLUE}
+    {player_name}, PRESS ENTER TO START THE GAME.\n\n\n    >>> """)
+
+    play_game = True
+    while True:
+        if play_game:
+            
+
+        player_choice = input(f"{gameover_qs}>>> ").lower()
+        if player_choice == "a":
+            print(f"\nYour choice is to keep playing\n")
+            play_game = True
+        elif player_choice == "b":
+            print(f"{Fore.RED}\nGame Finished...")
+            print(f"""{Fore.BLUE}
+            \nThanks for playing, {player_name.capitalize()}.
+            \nHope to see you again soon!\n""")
+            break
+        else:
+            print(f"""{Fore.MAGENTA}\n
+            That is not a valid option. Please try again.\n""")
+            play_game = False
+
+if __name__ == '__main__':
+    main()
