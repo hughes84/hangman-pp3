7d6d67fe6b772fed9757d332fa03d23aff2d9583
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-10-16 16:28:59 +0100

    Refactor: Fix spacing on rules

--- hangman_structure.py
+++ hangman_structure.py
@@ -127,19 +127,19 @@ game_details = [
  # rules
 
 """
-   H A N G M A N H A N G M         R U L E S           A N H A N G M A N H A H                                                                         
-   A                                                                         A           
+   H A N G M A N H A N G M         R U L E S           A N H A N G M A N H A H
+   A                                                                         A
    N                                                                         N
-   G          1.  Hangman is an objective based family game where a          G                                                         
+   G          1.  Hangman is an objective based family game where a          G
    M              word is to be guessed using only letters.                  M
    A          2.  You will have several attempts but can only have 6         A
    N              incorrect guesses.                                         N
    H          3.  With each incorrect guess the hangmans body will           H
    A              populate.                                                  A
    N          4.  When the hangmans head and full body appear you lose.      N
-   G          5.  For each correct letter the player receives 10pts.         G                             
-   M                                                                         M   
+   G          5.  For each correct letter the player receives 10pts.         G
+   M                                                                         M
    A                                                                         A
-   N H A N G M A N H A N G M A N H A N G M A N H A N G M A N H A N G M A N A N                                                                                                          
+   N H A N G M A N H A N G M A N H A N G M A N H A N G M A N H A N G M A N A N
 """
 ]
